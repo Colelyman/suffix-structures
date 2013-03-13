2d58e05177dd5db0ec2c59e079b7a80e0826384b
Cole Lyman <cole@colelyman.com>
2013-03-13 15:17:18 -0600

    Implemented intFind() and modified boolFind()

--- array.h
+++ array.h
@@ -15,9 +15,9 @@ public:
 	void generate();
 	string index(int position) const;
 	string print();
-	bool boolFind(string query);
-	bool boolFind(unsigned int start, unsigned int end, string query); // searches the suffix array and returns a bool
 	int intFind(string query); // searches the suffix array and returns an int of the index of the match in the reference; returns -1 if not found
+	int intFind(unsigned int start, unsigned int end, string query); 
+	bool boolFind(string query);
 private:
 	vector<int> sarray;
 	string reference;
@@ -63,20 +63,26 @@ string array::print() {
 	return temp;
 }
 
-bool array::boolFind(string query) {
-	return boolFind(0, sarray.size() - 1, query);
+int array::intFind(string query) {
+	return intFind(0, sarray.size() - 1, query);
 }
 
-bool array::boolFind(unsigned int start, unsigned int end, string query) { // end is intially equivalent to the size
+int array::intFind(unsigned int start, unsigned int end, string query) {
 	int mid = (end - start) / 2;
 	if(query == index(sarray.at(mid)))
-		return true;
+		return mid;
 	else if(mid <= 1)
-		return false;
+		return -1;
 	else if(query > index(sarray.at(mid)))
-		boolFind(mid + 1, end, query);
+		intFind(mid + 1, end, query);
 	else if(query < index(sarray.at(mid)))
-		boolFind(start, mid - 1, query);
+		intFind(start, mid - 1, query);
+	return -1;
+}
+
+bool array::boolFind(string query) {
+	if(intFind(0, sarray.size() - 1, query) >= 0)
+		return true;
 	else
 		return false;
 }
